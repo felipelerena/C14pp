19c03a3282b68c4aa60a3de4b4a4a293e641794b
Joaquín Sorianello <joac@gcoop.coop>
2011-11-26 14:39:41 -0300

    Esqueleto del javascript

---
+++ javascript/c14pp.js
@@ -0,0 +1,23 @@
+// Agarrar url y  consultar al webservice  para conseguir reglas de parseo de fechas
+// el json es de la forma {"target":"input['id=sarasa']", "regex":"s/frula/" }
+//
+
+function get_rule(url)
+{
+// Consulta el webservice y obtiene el selector jquery y la expresión regular para obtener la fecha
+
+
+
+}
+function match_date(date_string)
+{
+    var anio, mes, dia;
+}
+function get_timestamp(anio, mes, dia)
+{
+    var timestamp;
+}
+function get_image(time_stamp)
+{
+    
+}
